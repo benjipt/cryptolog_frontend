dc7fa15ba876d605d8aa37b0aa1a73fd41b3da8f
mitchell-mccracken <mpm5069@gmail.com>
2021-06-14 15:24:08 -0400

    created createUser.js

---
+++ cryptolog-react/src/components/createUser.js
@@ -0,0 +1,46 @@
+import {Component} from 'react'
+
+class CreateUser extends Component{
+    constructor(props){
+        super(props)
+            this.state = {
+                userName: '',
+                password: '',
+                passwordCheck: '',
+            }
+    }
+
+    // *** FUNCTIONS *** 
+    handleChange(event){
+        this.setState({ [event.currentTarget.id] : event.currentTarget.value })
+    }
+
+
+
+    render(){
+        return(
+            <div>
+                <h2>CREATE A NEW USER</h2>
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="userName" className="form-label">User Name</label>
+                        <input type="email" className="form-control" name="userName" id="userName" placeholder="kasmo.kramer@hotmail.com" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input type="string" className="form-control" name="password" id="password" placeholder="bosco" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="passwordCheck" className="form-label">Password</label>
+                        <input type="string" className="form-control" name="passwordCheck" id="passwordCheck" placeholder="bosco" />
+                    </div>
+                    <input type='submit' value='Create New User'></input>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+
+export default CreateUser
